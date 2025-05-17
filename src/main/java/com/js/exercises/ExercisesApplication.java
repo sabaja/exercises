809a6c020022895016fcb4f7877a4ab38f2af9dd
sabaja <jacoposabatini76@gmail.com>
2025-05-17 18:59:11 +0200

    Aggiunta variabile d'ambiente IS_INIT

--- src/main/java/com/js/exercises/ExercisesApplication.java
+++ src/main/java/com/js/exercises/ExercisesApplication.java
@@ -5,23 +5,26 @@ import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.domain.Specification;
 
-import java.util.List;
-
-
+@RequiredArgsConstructor
 @SpringBootApplication
 public class ExercisesApplication {
 
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ExercisesApplication.class);
+    private final StudentRepository studentRepository;
 
     @PersistenceContext
     public EntityManager entityManager;
+
+    @Value("${isInit}")
+    private boolean isInit;
 
     public static void main(String[] args) {
         SpringApplication.run(ExercisesApplication.class, args);
@@ -32,38 +35,41 @@ public class ExercisesApplication {
         return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
     }
 
-    @Autowired
-    StudentRepository studentRepository;
-
     @Bean
     public CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-            final var student = new Student();
-            final var school = new School();
-            student.setName("Gaia Sabatini");
-            school.setName("ICS Padre Puglisi - Laura Conti");
-
-            student.setSchool(school);
-            final var save = studentRepository.save(student);
-            log.info("save: {}", studentRepository.findById(save.getId()).orElseGet(() -> null));
-
-            final var jpaQueryFactory = jpaQueryFactory();
-            QStudent qStudent = QStudent.student;
-
-            final var res = jpaQueryFactory.selectFrom(qStudent)
-                    .where(qStudent.name.like("Jacopo%"))
-                    .fetchOne();
-//            log.info("res: {}", res);
-
-            Specification<Student> specification = StudentSpecification.joinSchool("HighSchool")
-                    .and(StudentSpecification.likeName("Jacopo"));
-
-            final var jacopo = studentRepository.findAll(specification);
-            log.info("Specification con Jacopo: {}", jacopo);
-
-            final var all = studentRepository.findAll();
-            log.info("Tutti: {}", all);
+            if (isInit) {
+                initData(studentRepository);
+            }
 
         };
+    }
+
+    private void initData(StudentRepository studentRepository) {
+        final var student = new Student();
+        final var school = new School();
+        student.setName("Gaia Sabatini");
+        school.setName("ICS Padre Puglisi - Laura Conti");
+
+        student.setSchool(school);
+        final var save = studentRepository.save(student);
+        log.info("save: {}", studentRepository.findById(save.getId()).orElseGet(() -> null));
+
+        final var jpaQueryFactory = jpaQueryFactory();
+        QStudent qStudent = QStudent.student;
+
+        final var res = jpaQueryFactory.selectFrom(qStudent)
+                .where(qStudent.name.like("Jacopo%"))
+                .fetchOne();
+//            log.info("res: {}", res);
+
+        Specification<Student> specification = StudentSpecification.joinSchool("HighSchool")
+                .and(StudentSpecification.likeName("Jacopo"));
+
+        final var jacopo = studentRepository.findAll(specification);
+        log.info("Specification con Jacopo: {}", jacopo);
+
+        final var all = studentRepository.findAll();
+        log.info("Tutti: {}", all);
     }
 }
