2b168981699b51575dabf5b2a0ce496d149b2815
sabaja <jacoposabatini76@gmail.com>
2025-11-23 19:30:52 +0100

    Aggiunto link a video min 11:00

--- src/main/java/com/js/exercises/spring/conditional/OnPropertyConfiguration.java
+++ src/main/java/com/js/exercises/spring/conditional/OnPropertyConfiguration.java
@@ -3,6 +3,8 @@ package com.js.exercises.spring.conditional;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
+
+//https://www.youtube.com/watch?v=IOWbaVf9M6U
 @Configuration
 @ConditionalOnProperty(
         value = "is.avaible",
