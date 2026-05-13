45fc33476cf537735a6881030e143a9fafafff43
U0N9369 <jacopo.sabatini@esternibisp.com>
2026-05-13 12:49:04 +0200

    Aggiunte classi per sincronizzazione

---
+++ src/main/java/com/js/exercises/purejava/threads/synchronizeer/Synch.java
@@ -0,0 +1,48 @@
+package com.js.exercises.purejava.threads.synchronizeer;
+
+// Provare a commentare le parti synchronized
+public class Synch {
+    static void main() {
+
+        CallMe target = new CallMe();
+        new Caller(target, "Hello");
+        new Caller(target, "Synchronized");
+        new Caller(target, "World");
+    }
+}
+
+class CallMe {
+    public synchronized void call(String msg) {
+
+        System.out.print("[" + msg);
+
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("]");
+    }
+}
+
+class Caller implements Runnable {
+
+    private final String msg;
+
+    private final CallMe target;
+
+    public Caller(CallMe t, String s) {
+        target = t;
+        msg = s;
+        new Thread(this).start();
+    }
+
+    public void run() {
+        synchronized (target) {
+            target.call(msg);
+        }
+    }
+
+}
+
